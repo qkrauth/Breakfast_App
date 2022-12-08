9a364da9193a3e4ac94d3a429e01012e1d949a68
Quinten Krauth <quintenkrauth19@gmail.com>
2022-12-08 17:32:18 -0500

    building out back end info

---
+++ breakfast-of-champs/server/seed/seed.js
@@ -0,0 +1,110 @@
+let bases = [
+    {
+        name: "Coke",
+        diet: false,
+    },
+    {
+        name: "Diet Coke",
+        diet: true,
+    },
+    {
+        name: "Mtn Dew",
+        diet: false,
+    },
+    {
+        name: "Diet Mtn Dew",
+        diet: true,
+    },
+    {
+        name: "Dr Pepper",
+        diet: false,
+    },
+    {
+        name: "Diet Dr Pepper",
+        diet: true,
+    },
+    {
+        name: "Sprite",
+        diet: false,
+    },
+    {
+        name: "Sprite Zero",
+        diet: true,
+    },
+    {
+        name: "Barqs Rootbeer",
+        diet: false,
+    },
+    {
+        name: "Diet Barqs Rootbeer",
+        diet: true,
+    },
+];
+
+let extras = [
+    {
+        name: "Vanilla Shot"
+    },
+    {
+        name: "Cherry Shot"
+    },
+    {
+        name: "Coconut Shot"
+    },
+    {
+        name: "Rasperry Shot"
+    },
+    {
+        name: "Strawberry Shot"
+    },
+    {
+        name: "Peach Shot"
+    },
+    {
+        name: "Blueberry Shot"
+    },
+    {
+        name: "Passionfruit Shot"
+    },
+    {
+        name: "Watermelon Shot"
+    },
+    {
+        name: "Pomegranite Shot"
+    },
+    {
+        name: "Pineapple Shot"
+    },
+    {
+        name: "Lemon Wedge"
+    },
+    {
+        name: "Lime Wedge"
+    },
+    {
+        name: "Coconut Cream"
+    },
+];
+
+let favorites = [
+    {
+        name: "Reactive Rootbeer",
+        baseId: 9,
+        extras: "[1, 14]"
+    },
+    {
+        name: "ConsoleLog Cola",
+        baseId: 1,
+        extras: "[2, 13]"
+    },
+    {
+        name: "Dr Postman",
+        baseId: 6,
+        extras: "[7, 14]"
+    },
+    {    
+        name: "getElementByI-Dew",
+        baseId: 3,
+        extras: "[8, 11, 12]"
+    },
+];
